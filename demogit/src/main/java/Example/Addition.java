6dc0cb78446042ad9c9d30c1d4f7141478198882
User <User@DESKTOP-TCDGSMG>
2022-04-22 10:05:57 +0530

    program

---
+++ demogit/src/main/java/Example/Addition.java
@@ -0,0 +1,14 @@
+package Example;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		int a,b,c;
+		a=10;
+		b=20;
+		c=a+b;
+		System.out.println(c);
+
+	}
+
+}
---
+++ demogit/src/main/java/Example/Example1.java
@@ -0,0 +1,10 @@
+package Example;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+
+	}
+
+}
